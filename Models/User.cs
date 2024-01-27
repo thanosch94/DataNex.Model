44c4647a3f55794a969efda139b7a417ed572ada
thanosch94 <chatzopoulosthanos@gmail.com>
2024-01-27 23:51:31 +0200

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Models
+{
+    [Table("datanex_users")]
+    public class User
+    {
+        public virtual Guid Id { get; set; }
+
+        public virtual string Name { get; set; }
+
+        public virtual string Email { get; set; }
+
+        public virtual string Password { get; set; }
+
+    }
+}
