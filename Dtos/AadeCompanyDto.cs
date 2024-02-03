581e4e401b4f5c46d3e79cdfec2479c7aa5be4ed
thanosch94 <chatzopoulosthanos@gmail.com>
2024-02-03 14:02:48 +0200

    Added Aade Service

---
+++ Dtos/AadeCompanyDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Dtos
+{
+    public class AadeCompanyDto
+    {
+        public string AFM { get; set; }
+        public string StopDate { get; set; }
+        public string PostalAddressNo { get; set; }
+        public string DoyDescr { get; set; }
+        public string Doy { get; set; }
+        public string Onomasia { get; set; }
+        public string LegalStatusDescr { get; set; }
+        public string RegistDate { get; set; }
+        public string DeactivationFlag { get; set; }
+        public string DeactivationFlagDescr { get; set; }
+        public string PostalAddress { get; set; }
+        public string FirmFlagDescr { get; set; }
+        public string CommerTitle { get; set; }
+        public string PostalAreaDescription { get; set; }
+        public string INiFlagDescr { get; set; }
+        public string PostalZipCode { get; set; }
+        public string FirmActDescr { get; set; }
+        public string FirmActKind { get; set; }
+        public string FirmActKindDescr { get; set; }
+        public string FirmActCode { get; set; }
+        public string ErrorDescr { get; set; }
+        public string ErrorCode { get; set; }
+    }
+}
