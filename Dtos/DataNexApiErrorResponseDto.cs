7f3b0beee3b4084a21d3de2417606046ebb5aa92
thanosch94 <chatzopoulosthanos@gmail.com>
2024-04-13 23:15:29 +0300

    Added Exception Handler

---
+++ Dtos/DataNexApiErrorResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Dtos
+{
+    public class DataNexApiErrorResponseDto
+    {
+        public int StatusCode { get; set; }
+
+        public string Message { get; set; }
+
+        public string Title { get; set; }
+        public string InnerExceptionMessage { get; set; }
+    }
+}
