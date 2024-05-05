50eba4c587f770872119adc9a0f6c52998fe25a7
thanosch94 <chatzopoulosthanos@gmail.com>
2024-05-05 21:47:39 +0300

    Added Login

---
+++ Dtos/LoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Dtos
+{
+    public class LoginDto
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }    
+    }
+}
