e86f8154036c7976c70e5fb6d77ec285086c111e
thanosch94 <chatzopoulosthanos@gmail.com>
2024-05-12 01:20:09 +0300

    Send user to client on login

---
+++ Dtos/ApiResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Dtos
+{
+    public class ApiResponseDto
+    {
+        public Object? Result { get; set; }
+
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+
+
+
+    }
+}
