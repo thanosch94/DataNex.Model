1c840e61e34707f17def4018706b4fa0feb9cdd7
thanosch94 <chatzopoulosthanos@gmail.com>
2024-07-02 23:32:03 +0300

    Added Warehouses

---
+++ Dtos/WareHouseDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Dtos
+{
+    public class WareHouseDto
+    {
+        public Guid Id { get; set; }
+
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDefault { get; set; }
+
+        public Guid? CompanyId { get; set; }
+
+    }
+}
---
+++ Models/WareHouse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Models
+{
+    [Table("datanex_warehouses")]
+    public class WareHouse:BaseModel
+    {
+        public WareHouse()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        [StringLength(10)]
+        public string Code { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public bool IsDefault { get; set; }
+
+        public Guid? CompanyId { get; set; }
+
+    }
+}
