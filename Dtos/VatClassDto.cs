c0e307a74922275c1995329ec0759af1344329a1
thanosch94 <chatzopoulosthanos@gmail.com>
2024-07-07 20:33:56 +0300

    Added Vat Classes

---
+++ Dtos/VatClassDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Dtos
+{
+    public class VatClassDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Abbreviation { get; set; }
+        public short Rate { get; set; }
+        public bool IsActive { get; set; }  
+
+    }
+}
---
+++ Models/VatClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Models
+{
+    [Table("datanex_vat_classes")]
+    public class VatClass:BaseModel
+    {
+        public VatClass()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(255)]
+        public string? Description { get; set; }
+        
+        [StringLength(10)]
+        public string? Abbreviation { get; set; }
+
+        public short Rate { get; set; } 
+
+
+    }
+}
