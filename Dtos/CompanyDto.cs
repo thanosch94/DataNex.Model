401c17fee1403c3228a21cfbe76bf4c7a80659d4
thanosch94 <chatzopoulosthanos@gmail.com>
2024-09-26 22:50:03 +0300

    Added Companies Model, Dto, Controller + Migration

---
+++ Dtos/CompanyDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Dtos
+{
+    public class CompanyDto
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDefault { get; set; }
+    }
+}
---
+++ Models/Company.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataNex.Model.Models
+{
+    [Table("datanex_companies")]
+    public class Company:BaseModel
+    {
+        public Company()
+        {
+            Id = Guid.NewGuid();
+
+        }
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsDefault { get; set; }
+
+    }
+}
