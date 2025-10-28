e86b7abb472d6e2fe6d035ab288d9f2d53f7fe93
thanosch94 <chatzopoulosthanos@gmail.com>
2025-10-28 14:21:33 +0200

    Added doctype transformations

---
+++ Dtos/DocTypeTransformationDto.cs
@@ -0,0 +1,14 @@
+﻿using DataNex.Model.Models;
+
+namespace DataNex.Model.Dtos
+{
+    public class DocTypeTransformationDto
+    {
+        public Guid Id { get; set; }
+        public Guid From { get; set; }
+        public Guid To { get; set; }
+        public Guid CompanyId { get; set; }
+
+
+    }
+}
---
+++ Models/DocTypeTransformation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataNex.Model.Models
+{
+    [Table("dn_doctypes_transformations")]
+    public class DocTypeTransformation:BaseModel
+    {
+        public DocTypeTransformation()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public Guid From { get; set; }
+        public Guid To { get; set; }
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; }
+
+    }
+}
